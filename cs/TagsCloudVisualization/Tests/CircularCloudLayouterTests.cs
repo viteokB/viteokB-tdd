ee289c4e303fb5316cd5baecd8572c8ee4c00ffd
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-11-22 00:46:17 +0500

    Поправил тестирование

--- cs/TagsCloudVisualization/Tests/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouterTests.cs
@@ -20,6 +20,8 @@ namespace TagsCloudVisualization.Tests
 
         private TagCloudImageGenerator _tagCloudImageGenerator;
 
+        private Size _errorImageSize;
+
         [SetUp]
         public void Setup()
         {
@@ -32,11 +34,21 @@ namespace TagsCloudVisualization.Tests
             _visualiser = new Visualiser(Color.Black, 1);
             
             _tagCloudImageGenerator = new TagCloudImageGenerator(_visualiser);
+
+            _errorImageSize = new Size(WIDTH, HEIGHT);
         }
 
-        [TearDown]
+       [TearDown]
         public void Teardown()
         {
+            if (TestContext.CurrentContext.Result.Outcome == ResultState.Failure)
+            {
+                using var errorBitmap = _tagCloudImageGenerator.CreateNewBitmap(_errorImageSize, _layouter.Rectangles);
+
+                var fileName = $"{TestContext.CurrentContext.Test.MethodName + Guid.NewGuid()}.png";
+                BitmapSaver.SaveToFail(errorBitmap, fileName);
+            }
+
             _visualiser.Dispose();
             _tagCloudImageGenerator.Dispose();
         }
@@ -52,7 +64,7 @@ namespace TagsCloudVisualization.Tests
         {
             var firstPoint = _layouter.RayMover.MoveRay().First();
 
-            firstPoint.Should().BeEquivalentTo(_layouter.RayMover.Center);
+            firstPoint.Should().BeEquivalentTo(_center);
         }
 
         [Test]
@@ -84,136 +96,55 @@ namespace TagsCloudVisualization.Tests
                 "Not valid size should be positive");
         }
 
-        [TestCase(0, 0)]
-        [TestCase(-2, 2)]
-        [TestCase(2, -2)]
-        [TestCase(-2, -2)]
-        public void CircularCloudLayouter_WhenCenterWrong_ThrowArgumentException(int x, int y)
-        {
-            var rayMover = new SpiralRayMover(new Point(x, y), 1, 5);
-
-            Assert.Throws<ArgumentException>(() => new CircularCloudLayouter(rayMover),
-                "Center Point should have positive X and Y");
-        }
-
-        [TestCase(0, 0)]
-        [TestCase(-2, 2)]
-        [TestCase(2, -2)]
-        [TestCase(-2, -2)]
-        public void CircularCloudLayouter_WhenNotValidSteps_ThrowArgumentException(int radiusStep, int angleSter)
-        {
-            var rayMover = new SpiralRayMover(new Point(10, 10), radiusStep, angleSter);
-
-            Assert.Throws<ArgumentException>(() => new CircularCloudLayouter(rayMover), 
-                "radiusStep and angleStep should be positive");
-        }
-
         [Test]
         public void CircularCloudLayouter_WhenAddFew_ShouldHaveSameCount()
         {
-            var currentBitmap = _tagCloudImageGenerator.CreateNewBitmap(new Size(WIDTH, HEIGHT),
-                _layouter,
-                () =>
-                {
-                    return SizeBuilder.Configure()
-                        .SetCount(5)
-                        .SetWidth(60, 80)
-                        .SetHeight(60, 80)
-                        .Generate();
-                });
+            var sizesList = GenerateSizes(5);
+            AddRectanglesToLayouter(sizesList);
 
-            try
-            {
-                _layouter.Rectangles.Should().HaveCount(5);
-            }
-            catch (Exception e)
-            {
-                var fileName = $"{TestContext.CurrentContext.Test.MethodName + Guid.NewGuid()}.png";
-
-                BitmapSaver.SaveToFail(currentBitmap, fileName);
-                currentBitmap.Dispose();
-
-                throw;
-            }
+            _layouter.Rectangles.Should().HaveCount(5);
         }
 
         [TestCase(1, 2, 3)]
-        public void CircularCloudLayouter_ShouldIncreaseRectangleCountCorrectly(int countBefore, 
-            int add, int countAfter)
+        public void CircularCloudLayouter_ShouldIncreaseRectangleCountCorrectly(int countBefore, int add, int countAfter)
         {
-            //Создали состояние countBefore
-            var currentBitmap = _tagCloudImageGenerator.CreateNewBitmap(new Size(WIDTH, HEIGHT),
-                _layouter,
-                () =>
-                {
-                    return SizeBuilder.Configure()
-                        .SetCount(countBefore)
-                        .SetWidth(60, 80)
-                        .SetHeight(60, 80)
-                        .Generate();
-                });
+            var countBeforeSizes = GenerateSizes(countBefore);
+            AddRectanglesToLayouter(countBeforeSizes);
 
-            //Создали состояние countAfter
-            _tagCloudImageGenerator.AddToCurrentImage(currentBitmap,
-                _layouter,
-                () =>
-                {
-                    return SizeBuilder.Configure()
-                        .SetCount(add)
-                        .SetWidth(60, 80)
-                        .SetHeight(60, 80)
-                        .Generate();
-                });
+            var addSizes = GenerateSizes(add);
+            AddRectanglesToLayouter(addSizes);
 
-            try
-            {
-                _layouter.Rectangles.Should().HaveCount(countAfter);
-            }
-            catch (Exception e)
-            {
-                var fileName = $"{TestContext.CurrentContext.Test.MethodName + Guid.NewGuid()}.png";
-
-                BitmapSaver.SaveToFail(currentBitmap, fileName);
-                currentBitmap.Dispose();
-
-                throw;
-            }
+            _layouter.Rectangles.Should().HaveCount(countAfter);
         }
 
         [TestCase(2)]
         [TestCase(30)]
         public void CircularCloudLayouter_WhenAddFew_RectangleNotIntersectsWithOtherRectangles(int count)
         {
-            //Создали состояние countBefore
-            var currentBitmap = _tagCloudImageGenerator.CreateNewBitmap(new Size(WIDTH, HEIGHT), _layouter,
-                () =>
-                {
-                    return SizeBuilder.Configure()
-                        .SetCount(count)
-                        .SetWidth(60, 80)
-                        .SetHeight(60, 80)
-                        .Generate();
-                });
+            var listSizes = GenerateSizes(count);
+            AddRectanglesToLayouter(listSizes);
 
-            try
+            foreach (var rectangle in _layouter.Rectangles)
             {
-                foreach (var rectangle in _layouter.Rectangles)
-                {
-                    _layouter.Rectangles
-                        //Не забываем исключить самого себя.....
-                        .Any(r => r.IntersectsWith(rectangle) && rectangle != r)
-                        .Should().BeFalse();
-                }
+                _layouter.Rectangles
+                    .Any(r => r.IntersectsWith(rectangle) && rectangle != r)
+                    .Should().BeFalse();
             }
-            catch (Exception e)
-            {
-                var fileName = $"{TestContext.CurrentContext.Test.MethodName + Guid.NewGuid()}.png";
+        }
 
-                BitmapSaver.SaveToFail(currentBitmap, fileName);
-                currentBitmap.Dispose();
+        private List<Size> GenerateSizes(int count)
+        {
+            return SizeBuilder.Configure()
+                .SetCount(count)
+                .SetWidth(60, 80)
+                .SetHeight(60, 80)
+                .Generate()
+                .ToList();
+        }
 
-                throw;
-            }
+        private void AddRectanglesToLayouter(List<Size> sizes)
+        {
+            sizes.ForEach(size => _layouter.PutNextRectangle(size));
         }
     }
 }
---
+++ cs/TagsCloudVisualization/Tests/SpiralRayMoverTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudVisualization.CloudClasses;
+
+namespace TagsCloudVisualization.Tests
+{
+    public class SpiralRayMoverTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(-2, 2)]
+        [TestCase(2, -2)]
+        [TestCase(-2, -2)]
+        public void CircularCloudLayouter_WhenNotValidSteps_ThrowArgumentException(int radiusStep, int angleSter)
+        {
+            Assert.Throws<ArgumentException>(() => new SpiralRayMover(new Point(10, 10), radiusStep, angleSter),
+                "radiusStep and angleStep should be positive");
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(-2, 2)]
+        [TestCase(2, -2)]
+        [TestCase(-2, -2)]
+        public void CircularCloudLayouter_WhenCenterWrong_ThrowArgumentException(int x, int y)
+        {
+            Assert.Throws<ArgumentException>(() => new SpiralRayMover(new Point(x, y), 1, 5),
+                "SpiralRayMover center Point should have positive X and Y");
+        }
+    }
+}
